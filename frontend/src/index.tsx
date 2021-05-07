1eb08a63dadf5d171ab24caf84e3a53c93832e5d
Vinicius Parente <vd.parente@hotmail.com>
2021-05-07 18:31:20 -0300

    Routes

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -4,12 +4,9 @@ import 'bootstrap/dist/css/bootstrap.css';
 import 'assets/css/styles.css';
 import App from './App';
 
-
-
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
-
